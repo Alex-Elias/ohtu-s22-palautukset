212ac5bb29f9e4c55c8eee0c6110dd614dcddf24
Alex-Elias <alex.pohjanpelto@gmail.com>
2022-11-28 23:13:18 +0200

    Add test to test that ostokset return one ostos for same items

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -78,3 +78,12 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 2)
+
+    def test_ostokset_return_one_ostos_when_two_of_same_items(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 1)
