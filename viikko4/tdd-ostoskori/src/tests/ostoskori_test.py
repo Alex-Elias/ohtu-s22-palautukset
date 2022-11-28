292da3bb593b96065f1be92326b0ff27481f7c8b
Alex-Elias <alex.pohjanpelto@gmail.com>
2022-11-28 22:59:17 +0200

    Add test that tests hinta returns correct value
    Added test that tests that hinta returns correct value when
    the shopping basket contains two of the same item

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -44,3 +44,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_hinta_returns_correct_value_with_two_same_items(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 6)
